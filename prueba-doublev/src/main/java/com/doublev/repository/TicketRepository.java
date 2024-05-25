f1410bd0a1d36eb44767d009fef2edc7349f83aa
christian sanchez <csanchez@innerconsulting.com>
2024-05-25 13:09:01 -0500

    a

--- prueba-doublev/src/main/java/com/doublev/repository/TicketRepository.java
+++
@@ -1,7 +0,0 @@
-package com.doublev.repository;
-
-import com.doublev.models.Ticket;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TicketRepository extends JpaRepository<Ticket, Long> {
-}
